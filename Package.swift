1990106a26a4973aef10dbc7ba4ea916a7aa5826
Bassem Tourky <bassem.tourky@deliveryhero.com>
2025-04-10 18:13:00 +0200

    fork https://bitbucket.org/usercentricscode/usercentrics-spm-ui/src at ea6cafd797f4c32271db3914c9d77d9c568700ff

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "UsercentricsUI",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "UsercentricsUI",
+            targets: ["UsercentricsUI"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Usercentrics", url: "https://bitbucket.org/usercentricscode/usercentrics-spm-sdk", from: "2.19.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UsercentricsUIFramework",
+            url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui/downloads/UsercentricsUI-2.19.0.xcframework.zip",
+            checksum: "44ec6c0c3d7897abfefc225a64f961169390970d4eda63ea8c45df468758c3d3"
+        ),
+        .target(
+            name: "UsercentricsUI",
+            dependencies: ["Usercentrics", "UsercentricsUIFramework"]
+        ),
+    ]
+)
